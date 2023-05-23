dc75c66f4fc1e39dd723a0c7c78731f7b6918b96
Vsevolod Pushin <pushin_95@mail.ru>
2023-05-23 14:06:09 +0400

    fixed pipe

--- src/app/pipes/temperature-formatting.pipe.ts
+++ src/app/pipes/temperature-formatting.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TemperatureFormattingPipe implements PipeTransform {
   transform(temperature: number): string {
     if (temperature) {
-      const sign = temperature < 0 ? '-' : '+';
+      const sign = temperature < 0 ? '' : '+';
       return `${sign}${Math.round(temperature)}º`;
     }
     return '';
